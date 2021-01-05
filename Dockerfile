5b4dfc3a8b3e64e80d46b2c2ce0ad679395fe82d
Abhay <abhay@tech.abhaytechnos.com>
2021-01-06 05:13:04 +0530

    added spec file and cnages in docker

--- Dockerfile
+++ Dockerfile
@@ -6,16 +6,36 @@
 # COPY . .
 # EXPOSE 4000
 # CMD ["node","app.js"]
-FROM node:12-alpine
+# FROM node:12-alpine
 
-WORKDIR /usr/app
+# WORKDIR /usr/app
 
+# COPY package*.json ./
+
+# RUN npm i --quiet
+
+# COPY . .
+
+# RUN npm install pm2 -g
+
+# CMD ["pm2-runtime", "app.js"]
+FROM node:carbon
+
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
 COPY package*.json ./
 
-RUN npm i --quiet
+RUN npm install
+# If you are building your code for production
+# RUN npm install --only=production
 
+# Bundle app source
 COPY . .
 
-RUN npm install pm2 -g
-
-CMD ["pm2-runtime", "app.js"]
+EXPOSE 8000
+CMD [ "npm", "start" ]
--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-express = require('express');
-morgan = require('morgan');
-path = require('path');
-methods = require('methods');
-bodyParser = require('body-parser');
-cors = require('cors');
-app.listen(8080, ()=> {
-  console.log('App running on port 8080  ')
+// express = require('express');
+// morgan = require('morgan');
+// path = require('path');
+// methods = require('methods');
+// bodyParser = require('body-parser');
+// cors = require('cors');
+// app.listen(8000, ()=> {
+//   console.log('App running on port 8080  ')
+// });
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('<h1 style="color:green;">Java Home App - version-10!!</h1> \n');
 });
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
